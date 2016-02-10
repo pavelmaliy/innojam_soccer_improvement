bf48e457bf2483f21570d96c85150ce0e89da299
pavelmaliy <pavel.maliy@sap.com>
2016-02-10 11:49:25 +0200

    commentators

--- innojam/ui/js/commentator.js
+++ innojam/ui/js/commentator.js
@@ -6,27 +6,23 @@
     var trackES = new Audio('/examples/assets/trackES.mp3');
     var trackAR = new Audio('/examples/assets/trackAR.mp3');
     var trackEN = new Audio('/examples/assets/trackEN.mp3');
-    var i = 0;
-    var changeTrack = function() {
 
-        if((i%3) === 0) {
-            trackAR.pause();
-            trackES.pause();
-            trackEN.play();
-        } else if ((i%3) === 1){
-            trackAR.pause();
-            trackES.play();
-            trackEN.pause();
-        } else {
-            trackAR.play();
-            trackES.pause();
-            trackEN.pause();
-        }
-        i++;
-    }
+  jQuery('.live').on('click',function() {
+      trackAR.pause();
+      trackES.pause();
+      trackEN.play();
+  }).bind(this);
 
-  jQuery('.change-commentator').on('click',function() {
-      changeTrack();
+  jQuery('.shlomi').on('click',function() {
+      trackAR.pause();
+      trackES.play();
+      trackEN.pause();
+  }).bind(this);
+
+  jQuery('.muhamad').on('click',function() {
+      trackAR.play();
+      trackES.pause();
+      trackEN.pause();
   }).bind(this);
 
 }());
