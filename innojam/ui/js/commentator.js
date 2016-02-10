ade0d86e808988895af44f036ebce27ef9c92a54
pavelmaliy <pavel.maliy@sap.com>
2016-02-10 15:18:01 +0200

    sound

--- innojam/ui/js/commentator.js
+++ innojam/ui/js/commentator.js
@@ -3,44 +3,44 @@
  */
 (function(){
 
-    var trackES = new Audio('/examples/assets/trackES.mp3');
-    var trackAR = new Audio('/examples/assets/trackAR.mp3');
-    var trackEN = new Audio('/examples/assets/trackEN.mp3');
-    var trackMeir = new Audio('/examples/assets/trackMeir.mp3');
-
+    var shlomi = new Audio('/examples/assets/shlomi.mp3');
+    var muhamad = new Audio('/examples/assets/muhamad.mp3');
+    var live = new Audio('/examples/assets/live.mp3');
+    var meir = new Audio('/examples/assets/meir.mp3');
+  //  live.play();
   jQuery('.live').on('click',function() {
-      trackAR.pause();
-      trackES.pause();
-      trackMeir.pause();
-      trackEN.play();
+      muhamad.pause();
+      shlomi.pause();
+      meir.pause();
+      live.play();
   }).bind(this);
 
   jQuery('.shlomi').on('click',function() {
-      trackAR.pause();
-      trackES.play();
-      trackMeir.pause();
-      trackEN.pause();
+      muhamad.pause();
+      shlomi.play();
+      meir.pause();
+      live.pause();
   }).bind(this);
 
   jQuery('.muhamad').on('click',function() {
-      trackAR.play();
-      trackES.pause();
-      trackMeir.pause();
-      trackEN.pause();
+      muhamad.play();
+      shlomi.pause();
+      meir.pause();
+      live.pause();
   }).bind(this);
 
   jQuery('.meir').on('click',function() {
-    trackAR.pause();
-    trackES.pause();
-    trackEN.pause();
-    trackMeir.play();
+    muhamad.pause();
+    shlomi.pause();
+    live.pause();
+    meir.play();
   }).bind(this);
 
   jQuery('.mute').on('click',function() {
-      trackAR.pause();
-      trackES.pause();
-      trackEN.pause();
-      trackMeir.pause();
+      muhamad.pause();
+      shlomi.pause();
+      live.pause();
+      meir.pause();
   }).bind(this);
 
 }());
